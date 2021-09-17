f06259a8ce42250213268dd235d732eb546f6eea
Ethan Low <ethanthelow@gmail.com>
2021-09-17 15:05:13 -0600

    hi

--- tsh.c
+++ tsh.c
@@ -134,8 +134,8 @@ void eval(char *cmdline)
                     if(argv[stdout_redir[i]] != "quit") {
                         int fileindex = ((2 * i) + 1);
                         fd[fileindex] = fopen(argv[stdout_redir[i]], "w");
-                    int outFileNum = fileno(fd[(2*i) + 1]);
-                    dup2(outFileNum, 1);
+                        int outFileNum = fileno(fd[(2*i) + 1]);
+//                        dup2(outFileNum, 1);
                     }
                 }
 
